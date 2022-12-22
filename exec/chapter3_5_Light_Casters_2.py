5ce6e1bdf412e48cc1f117fdefa68f673af7d118
anecjong <anecdotejong@gmail.com>
2022-12-22 16:07:25 +0900

    light casters

--- exec/chapter3_5_Light_Casters_2.py
+++ exec/chapter3_5_Light_Casters_2.py
@@ -5,6 +5,7 @@ import OpenGL.GL.shaders as shaders
 import numpy as np
 from ctypes import c_void_p
 from PIL import Image
+from math import sin, cos
 import glm
 from utils import ProcessUnit, Camera, Shader
 import os
@@ -23,6 +24,7 @@ out vec2 tex_coord;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+
 void main() {
     gl_Position = projection * view * model * vec4(position, 1.0f);
 
@@ -32,13 +34,24 @@ void main() {
 }
 """
 
+lamp_fragment_source = """
+#version 330 core
+out vec4 FragColor;
+
+uniform vec3 lamp_color;
+
+void main()
+{
+    FragColor = vec4(lamp_color, 1.0f);
+}
+"""
+
 fragment_source = """
 #version 330 core
 out vec4 frag_color;
 
 in vec3 normal_out;
 in vec3 frag_pos;
-in vec3 light_pos_out;
 in vec2 tex_coord;
 
 struct Material {
@@ -49,26 +62,32 @@ struct Material {
     float     shininess;
 }; 
 
-struct DirectionalLight {
-    // vec3 position;
-    vec3 direction;
+struct PointLight {
+    vec3 position;
   
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 uniform Material material;
-uniform DirectionalLight light;  
+uniform PointLight light;  
 
 void main() {
+    float distance = length(light.position - frag_pos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    
     // ambient
     vec3 material_diffuse = mix(texture(material.diffuse_1, tex_coord), texture(material.diffuse_2, tex_coord), material.mix_ratio).rgb;
     vec3 ambient = light.ambient * material_diffuse;
 
     // diffuse
     vec3 norm = normalize(normal_out);
-    vec3 light_dir = normalize(-light.direction);
+    vec3 light_dir = normalize(light.position - frag_pos);
     float diff = max(dot(norm, light_dir), 0.0);
     vec3 diffuse = light.diffuse * (diff * material_diffuse);
 
@@ -78,7 +97,7 @@ void main() {
     float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
     vec3 specular = light.specular * (spec * vec3(texture(material.specular, tex_coord)));
 
-    vec3 result = (ambient + diffuse + specular);
+    vec3 result = attenuation * (ambient + diffuse + specular);
     frag_color = vec4(result, 1.0f);
 }
 """
@@ -157,9 +176,13 @@ def main():
                                     shaders.compileShader(fragment_source, gl.GL_FRAGMENT_SHADER))
     shader = Shader(shader)
 
+    shader_lamp = shaders.compileProgram(shaders.compileShader(vertex_source, gl.GL_VERTEX_SHADER),
+                                    shaders.compileShader(lamp_fragment_source, gl.GL_FRAGMENT_SHADER))
+    shader_lamp = Shader(shader_lamp)
+
     # vao
-    vao_box = gl.glGenVertexArrays(1)
-    gl.glBindVertexArray(vao_box)
+    vao = gl.glGenVertexArrays(1)
+    gl.glBindVertexArray(vao)
 
     # vbo
     vbo = gl.glGenBuffers(1)
@@ -216,15 +239,9 @@ def main():
     
     gl.glClearColor(0.2, 0.3, 0.3, 1.0)
 
-    # texture shader
-    shader.use()
-    shader.uniform_int("material.diffuse_1", 0)
-    shader.uniform_int("material.specular", 1)
-    shader.uniform_int("material.diffuse_2", 0)
-
     # many cubes
     cube_positions = [
-        glm.vec3(0.0,   0.0,    0.0),
+        glm.vec3(0.0,   0.0,   0.0),
         glm.vec3(2.0,   5.0,   -15.0),
         glm.vec3(-1.5, -2.2,   -2.5),
         glm.vec3(-3.8, -2.0,   -12.3),
@@ -235,35 +252,68 @@ def main():
         glm.vec3(-1.3,  0.2,   -1.5),
     ]
 
+    process_unit.camera.change_camera_pos(glm.vec3(0, 0.0, 8.0))
+    process_unit.camera.change_fov(45)
+
     while not glfw.window_should_close(window):
+        # keyboard_input()
         process_unit.keyboard_input(window)
 
+        # view, projection info
         view = process_unit.camera.get_lookAt()
         projection = process_unit.camera.get_projection()
 
-        # projection / view
+        # lamp position
+        light_pos = glm.vec3(1.0, 1.0, 3.0)
+
+        # texture shader
+        shader.use()
+        shader.uniform_int("material.diffuse_1", 0)
+        shader.uniform_int("material.specular", 1)
+        shader.uniform_int("material.diffuse_2", 0)
+
+        # set projection / view
         shader.uniform_mat4("projection", projection)
         shader.uniform_mat4("view", view)
         
         # light
-        shader.uniform_vec3("light.direction", glm.vec3(-0.2, -1.0, -0.3))
+        shader.uniform_vec3("light.position", view * light_pos)
         shader.uniform_vec3("light.ambient",  glm.vec3(0.2))
-        shader.uniform_vec3("light.diffuse",  glm.vec3(0.4))
-        shader.uniform_vec3("light.specular", glm.vec3(0.8))
+        shader.uniform_vec3("light.diffuse",  glm.vec3(0.8))
+        shader.uniform_vec3("light.specular", glm.vec3(1.0))
 
-        # material shininess / mix ratio
+        shader.uniform_float("light.constant", 1.0)
+        shader.uniform_float("light.linear", 0.09)
+        shader.uniform_float("light.quadratic", 0.032)
+
+        # material shiness, mix ratio
         shader.uniform_float("material.shininess", 32)
         shader.uniform_float("material.mix_ratio", 0.5)
 
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-        gl.glBindVertexArray(vao_box)
+        gl.glBindVertexArray(vao)
 
         for i, cube_position in enumerate(cube_positions):
+            # model
             model = glm.translate(glm.mat4(1), cube_position)
             model = glm.rotate(model,(i+100.0) * glfw.get_time() / 200.0, glm.vec3(i%3, (i+1)%3, (i+2)%3))
 
             shader.uniform_mat4("model", model)
             gl.glDrawArrays(gl.GL_TRIANGLES, 0, int(vertices.size / 5))
+
+        # lamp
+        shader_lamp.use()
+
+        # projection / view / model
+        shader_lamp.uniform_mat4("projection", projection)
+        shader_lamp.uniform_mat4("view", view)
+        shader_lamp.uniform_vec3("position", light_pos)
+        shader_lamp.uniform_vec3("lamp_color", glm.vec3(1.0, 1.0, 1.0))
+
+        model = glm.translate(glm.mat4(1), light_pos)
+        model = glm.scale(model, glm.vec3(0.1, 0.1, 0.1))
+        shader_lamp.uniform_mat4("model", model)
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, int(vertices.size / 5))
 
         glfw.swap_buffers(window)
         glfw.poll_events()
